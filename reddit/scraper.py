559e1227ff2cfc58eec723b449ffde7244fe179b
Dave Gumba <dave_gumba@yahoo.com>
2016-08-24 20:58:38 -0400

    File system robust

--- reddit/scraper.py
+++ reddit/scraper.py
@@ -1,5 +1,7 @@
 # TODO:
-# Add cues
+# Look for folders
+# Store in list
+# Folder name: folder_names[-1] + 1
 
 from selenium import webdriver
 import urllib.request 
@@ -8,11 +10,19 @@ import os
 driver = webdriver.Firefox()
 #desired_url = input('What subreddit do you want to scrape the images of?\n')
 desired_url = 'earthporn'
-desired_folder = os.getcwd() + '/' + desired_url
 driver.get('http://imgur.com/r/'+desired_url)
 
-if not os.path.exists(desired_url):
-	os.makedirs(desired_url)
+folder_exists = False
+folder_version = 1
+
+while not folder_exists:
+	desired_folder = desired_url[:] + str(folder_version)
+
+	if os.path.exists(desired_folder):
+		folder_version += 1
+	else:
+		os.makedirs(desired_folder)
+		folder_exists = True
 
 imagelist = driver.find_element_by_id('imagelist') # Div of images
 links = imagelist.find_elements_by_tag_name('a') # Each link of post
@@ -29,6 +39,7 @@ for (i, source) in enumerate(hrefs):
 	src = img_div.find_element_by_tag_name('img').get_attribute('src')
 	fullfilename = os.path.join(desired_folder, str(i)+".jpg")
 	urllib.request.urlretrieve(src, fullfilename) # The line that saves
+	print('Saving ' + src + '....')
 
 
 driver.close()
