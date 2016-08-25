55b50073d6c6ab5d1709cb3344cc8499227c85ec
Dave Gumba <dave_gumba@yahoo.com>
2016-08-24 20:39:09 -0400

    Folder behavior working

--- reddit/scraper.py
+++ reddit/scraper.py
@@ -1,6 +1,5 @@
 # TODO:
-# Create a directory named desired_url
-# Save images in that
+# Add cues
 
 from selenium import webdriver
 import urllib.request 
@@ -9,13 +8,15 @@ import os
 driver = webdriver.Firefox()
 #desired_url = input('What subreddit do you want to scrape the images of?\n')
 desired_url = 'earthporn'
+desired_folder = os.getcwd() + '/' + desired_url
 driver.get('http://imgur.com/r/'+desired_url)
+
+if not os.path.exists(desired_url):
+	os.makedirs(desired_url)
 
 imagelist = driver.find_element_by_id('imagelist') # Div of images
 links = imagelist.find_elements_by_tag_name('a') # Each link of post
 
-# I couldn't make this into a list comprehension because
-# Apparently, links isn't an iterable
 hrefs = []
 for link in links:
 	href = link.get_attribute("href")
@@ -26,7 +27,8 @@ for (i, source) in enumerate(hrefs):
 	driver.get(source)
 	img_div = driver.find_element_by_class_name('post-image')
 	src = img_div.find_element_by_tag_name('img').get_attribute('src')
-	urllib.request.urlretrieve(src, str(i)+".jpg") # The line that saves
+	fullfilename = os.path.join(desired_folder, str(i)+".jpg")
+	urllib.request.urlretrieve(src, fullfilename) # The line that saves
 
 
 driver.close()
