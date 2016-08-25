f80e057d3dde81febe36932ce8e8586eb1f854ad
Dave Gumba <dave_gumba@yahoo.com>
2016-08-24 20:28:06 -0400

    This works

--- reddit/scraper.py
+++ reddit/scraper.py
@@ -1,21 +1,32 @@
+# TODO:
+# Create a directory named desired_url
+# Save images in that
+
 from selenium import webdriver
-import urllib.request
+import urllib.request 
+import os
 
 driver = webdriver.Firefox()
 #desired_url = input('What subreddit do you want to scrape the images of?\n')
 desired_url = 'earthporn'
 driver.get('http://imgur.com/r/'+desired_url)
 
-# All images are in a div with id "imagelist"
-# Each image in a div with class "post"
-# Each "post" div has a <a> tag with a href
-# GET THAT HREF
-# download it
-
 imagelist = driver.find_element_by_id('imagelist') # Div of images
-posts = imagelist.find_elements_by_class_name('post') # Each post
+links = imagelist.find_elements_by_tag_name('a') # Each link of post
 
-for post in posts:
+# I couldn't make this into a list comprehension because
+# Apparently, links isn't an iterable
+hrefs = []
+for link in links:
+	href = link.get_attribute("href")
+	if desired_url in href:
+		hrefs.append(href)
+
+for (i, source) in enumerate(hrefs):
+	driver.get(source)
+	img_div = driver.find_element_by_class_name('post-image')
+	src = img_div.find_element_by_tag_name('img').get_attribute('src')
+	urllib.request.urlretrieve(src, str(i)+".jpg") # The line that saves
 
 
 driver.close()
